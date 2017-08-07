b1704fad428f351637c34e078cfdd83a94b5cbe9
Maude <mualth@gmail.com>
2017-08-06 22:28:50 -0400

    working on the user auth models, created files for the search and results components and pages

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const config = require('./config');
+
+//connect to database
+require('./server/models').connect(config.dbUri);
 
 const app = express();
 
@@ -8,9 +13,23 @@ app.use(express.static('./client/dist/'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+//passport
+app.use(passport.initialize());
+const localSignUp = require('./server/passport/local-signup');
+const localLogin = require('./server/passport/local-login');
+passport.use('local-signup', localSignUp);
+passport.use('local-login', localLogin);
+
+const authMiddle = require('./server/middleware/auth-check');
+app.use('/api', AuthCheckMiddleware);
+
+
 // routes
 const authRoutes = require('./server/routes/auth');
+const apiRoutes = require('./server/routes/api');
 app.use('/auth', authRoutes);
+app.use('/api', apiRoutes);
 
 app.listen(3000, () => {console.log('Server is running on http://localhost:3000');
 });
